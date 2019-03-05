735eb76b3d7ae6bc3e20bd9b2f8ba6f3b867b981
Nhlalala <chanellesesh@gmail.com>
2019-03-05 15:44:34 +0200

    states fully fuctional, checking and unchecking works well too.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,42 @@ import Todos from "./components/Todos";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Get a job",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Finish this video",
+        completed: false
+      }
+    ]
+  };
+
+  //Toggle check box on complete
+
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
