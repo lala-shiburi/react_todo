ee40e63ba68611cbf7b00f16a9e924c8fd258e6a
Nhlalala <chanellesesh@gmail.com>
2019-03-06 01:54:37 +0200

    Routes all set up

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { link } from "fs";
 
 function Header() {
   return (
     <header style={headerStyle}>
       <h1>TodoList</h1>
+      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/About">About</Link>
     </header>
   );
 }
@@ -14,5 +17,8 @@ const headerStyle = {
   textAlign: "center",
   padding: "10px"
 };
-
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none'
+}
 export default Header;
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a app designed to help you get things done</p>
+        </React.Fragment>
+    )
+}
+
+
+export default About;
